0e9c590f13f72bb933c3e846741d9b799d76ea24
Baole444 <nguyenbaole246@gmail.com>
2025-04-13 22:56:47 +0700

    Protect entity on claimed chunk
    Tweak mixin for ProjectileEntity to protect entity also

--- src/main/java/Sky/Cat/CTC/mixin/ProjectileEntityMixin.java
+++ src/main/java/Sky/Cat/CTC/mixin/ProjectileEntityMixin.java
@@ -4,6 +4,7 @@ import Sky.Cat.CTC.Main;
 import Sky.Cat.CTC.chunk.ChunkManager;
 import Sky.Cat.CTC.chunk.ChunkPosition;
 import Sky.Cat.CTC.permission.PermType;
+import net.minecraft.block.ButtonBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -11,6 +12,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import org.joml.Math;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,18 +26,20 @@ public abstract class ProjectileEntityMixin {
     @Inject(method = "onCollision", at = @At("HEAD"), cancellable = true)
     private void stopInteractionsOnCollision(HitResult hitResult, CallbackInfo ci) {
         try {
-            HitResult.Type type = hitResult.getType();
-
-            if (type != HitResult.Type.BLOCK && type != HitResult.Type.ENTITY) return;
-
             ProjectileEntity projectile = (ProjectileEntity) (Object) this;
 
             // Processed by server only.
             if (projectile.getWorld().isClient()) return;
 
-            BlockHitResult blockHitResult = (BlockHitResult) hitResult;
+            HitResult.Type type = hitResult.getType();
 
-            BlockPos blockPos = blockHitResult.getBlockPos();
+            if (type != HitResult.Type.BLOCK && type != HitResult.Type.ENTITY) return;
+
+            int x = (int) Math.floor(hitResult.getPos().getX());
+
+            int z = (int) Math.floor(hitResult.getPos().getZ());
+
+            BlockPos blockPos = new BlockPos(x, 0, z);
 
             ChunkManager chunkManager = ChunkManager.getInstance();
             ChunkPosition chunkPosition = new ChunkPosition(blockPos, projectile.getWorld().getRegistryKey());
@@ -47,10 +51,6 @@ public abstract class ProjectileEntityMixin {
                 boolean hasPermission = false;
 
                 if (owner instanceof PlayerEntity player) {
-
-                    // Skip check if admin bypass is enabled.
-                    if (Main.ADMIN_BYPASS && player.hasPermissionLevel(2)) return;
-
                     hasPermission = chunkManager.hasPermission(player, blockPos, projectile.getWorld().getRegistryKey(), PermType.INTERACT);
 
                     if (!hasPermission) {
@@ -71,7 +71,42 @@ public abstract class ProjectileEntityMixin {
                 }
             }
         } catch (Exception e) {
-            Main.LOGGER.error("Error in ProjectileEntityMixin: ", e);
+            Main.LOGGER.error("Error in ProjectileEntityMixin.stopInteractionsOnCollision: ", e);
+        }
+    }
+
+    @Inject(method = "onBlockHit", at = @At("HEAD"), cancellable = true)
+    private void stopBlockLogicAfterCollision(BlockHitResult blockHitResult, CallbackInfo ci) {
+        try {
+            ProjectileEntity projectile = (ProjectileEntity) (Object) this;
+
+            // Processed by server only.
+            if (projectile.getWorld().isClient()) return;
+
+            BlockPos blockPos = blockHitResult.getBlockPos();
+
+            ChunkManager chunkManager = ChunkManager.getInstance();
+            ChunkPosition chunkPosition = new ChunkPosition(blockPos, projectile.getWorld().getRegistryKey());
+
+            if (chunkManager.isChunkClaimed(chunkPosition)) {
+                Entity owner = this.getOwner();
+
+                boolean hasPermission = false;
+
+                if (owner instanceof PlayerEntity player) {
+
+                    hasPermission = chunkManager.hasPermission(player, blockPos, projectile. getWorld().getRegistryKey(), PermType.INTERACT);
+                }
+
+                if (!hasPermission) {
+                    ChunkManager.LOGGER.debug("| Block logic for projectile intercepted:");
+                    ChunkManager.LOGGER.debug("| Position: {}", chunkPosition);
+
+                    ci.cancel();
+                }
+            }
+        } catch (Exception e) {
+            Main.LOGGER.error("Error in ProjectEntityMixin.stopBlockLogicAfterCollision: ", e);
         }
     }
 }
